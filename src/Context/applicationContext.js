9ba0b8da8235997a7f7315c9dd4fb1fe7e531dff
your-name <abdellamahmoud98@gmail.com>
2024-12-11 13:19:10 -0500

    added 12.5: Weather App – Application Context and Install React Cookie

---
+++ src/Context/applicationContext.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
+
+const AppContext = React.createContext();
+
+const AppContextProvider = ({ children }) => {
+    const [cookies, setCookie, removeCookie] = useCookies(["appToken"]);
+    const [userSessionData, setUserSessionData] = useState(undefined);
+    const setSession = (userData) => {
+        setCookie("userData", userData, {
+          path: "/",
+          maxAge: 900, //15minutes
+        });
+    };
+    const getSession = () => {
+        const userData = cookies.userData || null;
+        return userData;
+       };
+       const setUserData = (userData) => setUserSessionData(userData);
+       const getUserData = () => userSessionData;
+       const logout = () => {
+        removeCookie("userData", { path: "/" });
+        setUserData(undefined);
+    };
+    return<AppContext.Provider
+    value={{
+      setSession,
+      getSession,
+      setUserData,
+      getUserData,
+      logout,
+    }}
+    >
+    {children}
+</AppContext.Provider>
+    };
+
+    export { AppContext };
+export default AppContextProvider;
