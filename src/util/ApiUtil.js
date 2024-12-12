7296e4d02e7f630141780c3dca785c5ddf3fc9d4
abduls1997 <abdulkadir.salad1996@gmail.com>
2024-12-12 13:31:31 -0500

    Added Integrate Login API with Login Component

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -23,14 +23,14 @@ export const signUpApi = async (
 ) => {
   let response = frameResponse();
   try {
-    const url =  `${API_BASE_URL}/user/signup`;
-    const apiResponse = await axios.post( url,  {
-       firstName,
-       lastName,
-       username,
-       phone,
-       emailId,
-       password,
+    const url = `${API_BASE_URL}/user/signup`;
+    const apiResponse = await axios.post(url, {
+      firstName,
+      lastName,
+      username,
+      phone,
+      emailId,
+      password,
     });
     if (apiResponse.status === 200) {
       response = frameResponse(1);
@@ -44,25 +44,47 @@ export const signUpApi = async (
     return response;
   }
 };
-  //integrate the verifyemail api
-  export const verifyEmailApi = async (token) => {
-    let response = frameResponse();
-  
-    try {
-      const url = `${API_BASE_URL}/user/verify/email`;
-      const apiResponse = await axios.get(url, {
-        headers: { Authorization: frameToken(token) },
-      });
-  
-      if (apiResponse.status === 200) {
-        response = frameResponse(1, apiResponse.data);
-      }
-    } catch (err) {
-      if (err.response) {
-        response = frameResponse(0, err.response.data.message);
-      }
-      console.log(err);
-    } finally {
-      return response;
+//integrate the verifyemail api
+export const verifyEmailApi = async (token) => {
+  let response = frameResponse();
+
+  try {
+    const url = `${API_BASE_URL}/user/verify/email`;
+    const apiResponse = await axios.get(url, {
+      headers: { Authorization: frameToken(token) },
+    });
+
+    if (apiResponse.status === 200) {
+      response = frameResponse(1, apiResponse.data);
     }
-  };
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
+
+//login API from the backend
+export const loginApi = async (username, password) => {
+  let response = frameResponse();
+  try {
+    const url = `${API_BASE_URL}/user/login`;
+    const apiResponse = await axios.post(url, { username, password });
+    if (apiResponse.status === 200) {
+      const payLoad = {
+        token: apiResponse.headers.authorization, // the authorization token is present in response headers
+        username: apiResponse.data.username,
+      };
+      response = frameResponse(1, payLoad);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+  } finally {
+    return response;
+  }
+};
