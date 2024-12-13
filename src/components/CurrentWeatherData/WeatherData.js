66c3eef292bd6fbcf5a50792e5a1dc091fd3c62c
your-name <abdellamahmoud98@gmail.com>
2024-12-13 15:34:09 -0500

    added 12.31: Weather App – Session Token Expiration Component

--- src/components/CurrentWeatherData/WeatherData.js
+++ src/components/CurrentWeatherData/WeatherData.js
@@ -12,6 +12,8 @@ import DisplayWeatherData from "./DisplayWeatherData";
 // Import the getWeatherDataApi function from the ApiUtil file
 import { getWeatherDataApi } from "../../util/ApiUtil";
 
+
+
 const WeatherData = () => {
   // - data: used to store weather data fetched from the API
   const [data, setData] = useState(null);
@@ -30,6 +32,8 @@ const WeatherData = () => {
   // the default weather which comes up in Weather record don't save to database
   const [save, setSave] = useState(false);
 
+
+
   // Define the handleInput this for the search bar 
   const handleInput = (e) => {
     setInputValue(e.target.value);
@@ -39,15 +43,17 @@ const WeatherData = () => {
   const handleSubmit = (e) => {
     // if input value is not empty
     if (inputValue !== "") {
+
     // set location
-    setLocation (inputValue); setSave(true); // save the weather record to the database when user search for a city
+    setLocation (inputValue); 
+    setSave(true); // save the weather record to the database when user search for a city
     }
 
     // select input
     const input = document. querySelector ("input");
 
     // if input value is empty
-    if (input.value === "") E
+    if (input.value === "") 
     toast("Please enter a city name");
     // set animate to true
     setAnimate(true);
@@ -61,101 +67,49 @@ const WeatherData = () => {
     // prevent defaults
     e. preventDefault();
     };
-  };
+   
 
 
 
-  // fetch the data
-useEffect (() => {
-  // set loading to true
-  setLoading (true) ;
+//   // fetch the data
+// useEffect (() => {
+//   // set loading to true
+//   setLoading (true) ;
 
-  getWeatherDataApi(currentUser.token, location, save)
-   .then ((res) => {
-  // set the data after 100 ms
-  setTimeout (() => {
-    setData (res.data) ;
+//   getWeatherDataApi(currentUser.token, location, save)
+//    .then ((res) => {
+//   // set the data after 100 ms
+//   setTimeout (() => {
+//     setData (res.data) ;
 
-  // set loading to false
-  setLoading(false);
-   }, 100);
-  })
-  .catch ((err) => { 
-  setLoading(false);
-  setErrorMsg(err);
-  console.log(err);
-  if (err.response.data.httpStatusCode === 401){
-    setTokenExpired(true);
-  }
-  }) ;
-}, [location]);
-
-
-// error message
-useEffect(() => {
-  const timer = setTimeout(() => {
-  setErrorMsg("");
-}, 3000);
-  // clear timer
-  return () => clearTimeout (timer);
-  }, [errorMsg]);
+//   // set loading to false
+//   setLoading(false);
+//    }, 100);
+//   })
+//   .catch ((err) => { 
+//   setLoading(false);
+//   setErrorMsg(err);
+//   console.log(err);
+//   if (err.response.data.httpStatusCode === 401){
+//     setTokenExpired(true);
+//   }
+//   }) ;
+// }, [location]);
 
 
-  // if data is false show the loading icon
-  if (!data) {
-    return (
-      <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-coljustify-center items-center"></div>
-        <div>
-          < LoadingIndicator />
-      </div>
-    </div>
-    );
-  }
-     
+// // error message
+// useEffect(() => {
+//   const timer = setTimeout(() => {
+//   setErrorMsg("");
+// }, 3000);
+//   // clear timer
+//   return () => clearTimeout (timer);
+//   }, [errorMsg]);
+
+
+//   // if data is false show the loading icon
   
-
-  return (
-    <div className="w-full min-h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0">
-      {errorMsg && (
-        <div className="w-full max-w-[90vw] lg:max-w-[450px] bg-purple-800 text-white absolute top-2 lg:top-30 p-3 capitalize rounded-md">
-          {`${errorMsg.response.data.message}`}
-        </div>
-      )}
-      {/* form */}
-      <form
-        className={`${animate ? "animate-shake" : "animate-none"
-          } h-16 bg-black/30 w-full max-w-[700px]
-      rounded-full backdrop-blur-[32px] mb-10`}
-      >
-        <div className="h-full relative flex items-center justify-between p-2">
-          <input
-            onChange={(e) => handleInput(e)}
-            className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
-            type="text"
-            placeholder="Search by city"
-          />
-          <button
-            onClick={(e) => handleSubmit(e)}
-            className="bg-purple-700  hover:bg-purple-600 focus:bg-purple-600 w-20 h-12 rounded-full flex justify-center items-center transition"
-          >
-            <IoMdSearch className="text-2xl text-white" />
-          </button>
-        </div>
-      </form>
-      {/* card */}
-      <div className="w-full max-w-[700px] bg-black/20 min-h-[584px] text-purple-900 backdrop-blur-[80px] rounded-[32px] py-12 px-3">
-        {loading ? (
-          <div className="w-full h-full flex justify-center items-center">
-            <LoadingIndicator />
-          </div>
-        ) : (
-          <div>
-            <DisplayWeatherData apiResponse={data} />
-          </div>
-        )}
-      </div>
-    </div>
-);
+  
+// }
 }
-
-export default WeatherData
+export default WeatherData;
