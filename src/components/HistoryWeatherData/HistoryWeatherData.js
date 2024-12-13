f43c8d51a3d18ea0f05985fe3560c7c908b1a78c
Emmanuel Gbaragbo <egbaragbo@gmail.com>
2024-12-13 14:45:58 -0500

    integrateWeatherApi

--- src/components/HistoryWeatherData/HistoryWeatherData.js
+++ src/components/HistoryWeatherData/HistoryWeatherData.js
@@ -1,16 +1,67 @@
-
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NoHistoryWeatherPresent from './NoHistoryWeatherPresent'
+import toast from 'react-hot-toast'
+import { getHistoryWeatherDataApi } from '../../util/ApiUtil'
+import DisplayWeatherData from '../CurrentWeatherData/WeatherData'
+import LoadingIndicator from '../LoadingIndicator/LoadingIndicator'
 
+const HistoryWeatherData = ({ currentUser }) => {
+  const [results, setResults] = useState([])
+  const [tokenExpired, setTokenExpired] = useState(false)
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-const HistoryWeatherData = () => {
-  return (
-    <div>
-  <NoHistoryWeatherPresent/> 
+  const getMyResults = async () => {
+    setLoading(true)
 
-      HistoryWeatherData
-    </div>
-  );
-};
+    const apiResponse = await getHistoryWeatherDataApi(currentUser.token)
 
-export default HistoryWeatherData;
+    if (apiResponse.status === 1 && apiResponse.payLoad.length > 0) {
+      setResults(apiResponse.payLoad)
+      setData(true)
+      setLoading(false)
+    } else if (apiResponse.status === 1 && apiResponse.payLoad.length === 0) {
+      setData(false)
+      setLoading(false)
+    } else if (
+      apiResponse.status === 0 &&
+      apiResponse.payLoad === 'Token has Expired'
+    ) {
+      setTokenExpired(true)
+      setLoading(false)
+    } else {
+      toast(apiResponse.payLoad)
+    }
+  }
+
+  useEffect(() => {
+    getMyResults()
+  }, [])
+
+  if (loading) {
+    return <LoadingIndicator />
+  }
+
+  if (data === false) {
+    return (
+      <div flex items-center justify-center>
+        <NoHistoryWeatherPresent />
+      </div>
+    )
+  } else {
+    return (
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ga-4 p-4'>
+        {results.map((item, index) => {
+          ;<div
+            key={index}
+            className='bg-black/20 text-purple-900 backdrop-blur-[80px] py-12 px-6 rounded-lg overflow-hidden'
+          >
+            <DisplayWeatherData apiResponse={item} />
+          </div>
+        })}
+      </div>
+    )
+  }
+}
+
+export default HistoryWeatherData
